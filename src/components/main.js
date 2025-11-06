c81dfa6f0fdc84c53c08e5319d0abfab7988e594
heclates <goringorin123@gmail.com>
2025-11-06 23:32:13 +0100

    make introduction info, prepare courses block, make scroll navigation to form

--- src/components/main.js
+++ src/components/main.js
@@ -3,14 +3,78 @@ import DOMPurify from 'dompurify';
 export function createMain(submitHandler) {
   const main = document.createElement('main');
   main.setAttribute('role', 'main');
+  main.style.display = 'flex'
+  main.style.flexDirection = 'column'
+  main.style.gap = '200vh'
 
+  //Introduction | Main page
+  const titleMain = document.createElement('article')
+  titleMain.className = 'title--main'
+
+  const titleInfo = document.createElement('section')
+  titleInfo.className = 'title--main__info'
+  titleMain.appendChild(titleInfo)
+
+  const titleTitle = document.createElement('h1')
+  titleTitle.classList = 'title--main__text'
+  titleTitle.textContent = 'Творите, вдохновляйтесь, создавайте'
+  titleInfo.appendChild(titleTitle)
+
+  const titleText = document.createElement('p')
+  titleText.textContent = 'Художественная студия, где каждый может раскрыть свой творческий потенциал — дети, взрослые, начинающие и опытные художники.'
+  titleInfo.appendChild(titleText)
+
+  const titleBtnToForm = document.createElement('button')
+  titleBtnToForm.textContent = 'Запись на занятие'
+  titleInfo.appendChild(titleBtnToForm)
+  titleBtnToForm.addEventListener('click', function() {
+    const targetForm = document.getElementById('sectionForm')
+
+    if (targetForm) {
+      targetForm.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    }
+  })
+
+  const titleImg = document.createElement('img')
+  titleImg.src = '🎨'
+  titleImg.style.width = '800px'
+  titleImg.alt = 'logo Art-Studio'
+  titleImg.loading = 'lazy'
+  titleInfo.appendChild(titleImg)
+
+  //Courses
+  const coursesArticle = document.createElement('article')
+  
+  const coursesTitle = document.createElement('h2')
+  coursesTitle.textContent = 'Наши направления'
+
+  const corsesText = document.createElement('p')
+  corsesText.textContent = 'Выберите подходящий курс для себя или своего ребёнка'
+
+  const coursesSectionChild = document.createElement('section')
+
+  const coursesSectionJunior = document.createElement('section')
+
+  const coursesSectionAdult = document.createElement('section')
+
+  const coursesPrepareToHigh = document.createElement('section')
+
+  const coursesKeramika = document.createElement('section')
+
+  const coursesComboLesson = document.createElement('section')
+
+  //Rezervation block
   const section = document.createElement('section');
   section.setAttribute('aria-labelledby', 'reservation-title');
+  section.id = 'sectionForm'
 
-  const title = document.createElement('h2');
-  title.id = 'reservation-title';
-  title.textContent = 'Форма резервации';
-  section.appendChild(title);
+  const title_rezervation = document.createElement('h2');
+  title_rezervation.id = 'reservation-title';
+  title_rezervation.textContent = 'Форма резервации';
+  section.appendChild(title_rezervation);
 
   const form = document.createElement('form');
   form.id = 'reservationForm';
@@ -112,6 +176,7 @@ export function createMain(submitHandler) {
     form.reset();
   });
 
+  main.appendChild(titleMain)
   section.appendChild(form);
   main.appendChild(section);
   return main;
