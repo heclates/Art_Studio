eaeb22abffd29793ffc2e58e46acbd8754bf9309
heclates <goringorin123@gmail.com>
2025-12-26 22:07:13 +0100

    move terms privacy to public dir

--- src/i18n/footer/en.js
+++ src/i18n/footer/en.js
@@ -2,10 +2,10 @@
 export const footerEN = {
   navItems: [
     { href: '#', text: 'Home' },
-    { href: 'terms.html', text: 'Terms' },
     { href: '#courses', text: 'Courses' },
     { href: '#contacts', text: 'Contacts' },
-    { href: 'privacy.html', text: 'Privacy Policy' }
+    { href: '/terms.html', text: 'Terms' },
+    { href: '/privacy.html', text: 'Privacy Policy' }
   ],
   copyright: '© 2025 Your Company. All rights reserved.'
 };
--- src/i18n/footer/ru.js
+++ src/i18n/footer/ru.js
@@ -2,10 +2,10 @@
 export const footerRU = {
   navItems: [
     { href: '#', text: 'Главная' },
-    { href: 'terms.html', text: 'Услуги' },
     { href: '#courses', text: 'Курсы' },
     { href: '#contacts', text: 'Контакты' },
-    { href: 'privacy.html', text: 'Политика конфиденциальности' }
+    { href: '/terms.html', text: 'Услуги' },
+    { href: '/privacy.html', text: 'Политика конфиденциальности' }
   ],
   copyright: '© 2025 Ваша Компания. Все права защищены.'
 };
