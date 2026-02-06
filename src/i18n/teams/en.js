e97ca046f09f9c328339e8aa881dd7dd87218651
heclates <goringorin123@gmail.com>
2026-02-06 21:37:50 +0100

    check src for teams pic

--- src/i18n/teams/en.js
+++ src/i18n/teams/en.js
@@ -3,13 +3,13 @@ export const teamsEN = {
     text: 'Seznamte se s naším týmem lektorů',
     list: [
         {
-            img: './assets/img_teams/ekaterina.jpg', 
+            img: '/assets/img_teams/ekaterina.JPG', 
             alt: 'Ekaterina, zakladatelka a lektorka Aleksandrova Art Studio',
             name: 'Ekaterina',
             text: 'Zakladatelka a lektorka Aleksandrova Art Studio.'
         },
         {
-            img: './assets/img_teams/kristina.jpg', 
+            img: '/assets/img_teams/kristina.JPG', 
             alt: 'Kristina, lektorka Aleksandrova Art Studio',
             name: 'Kristina',
             text: 'Lektorka Aleksandrova Art Studio.'
--- src/i18n/teams/ru.js
+++ src/i18n/teams/ru.js
@@ -5,12 +5,14 @@ export const teamsRU = {
     text: 'Познакомьтесь с нашей командой преподавателей',
     list: [
         {
-            img: '/assets/img_teams/ekaterina.jpg', alt: 'Екатерина, основательница и преподаватель Aleksandrova Art Studio',
+            img: '/assets/img_teams/ekaterina.JPG', 
+            alt: 'Екатерина, основательница и преподаватель Aleksandrova Art Studio',
             name: 'Екатерина',
             text: 'Основатель и преподаватель Aleksandrova Art Studio.'
         },
         {
-            img: '/assets/img_teams/kristina.jpg', alt: 'Кристина, преподаватель Aleksandrova Art Studio',
+            img: '/assets/img_teams/kristina.JPG', 
+            alt: 'Кристина, преподаватель Aleksandrova Art Studio',
             name: 'Кристина',
             text: 'Преподаватель Aleksandrova Art Studio.'
         },
