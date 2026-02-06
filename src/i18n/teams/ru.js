3a6f28b4a1bba55a374f081bad34fc54eb7612ae
heclates <goringorin123@gmail.com>
2026-02-06 21:33:30 +0100

    fix media font weight, change i18n/teams source for pic

--- src/i18n/teams/ru.js
+++ src/i18n/teams/ru.js
@@ -5,12 +5,12 @@ export const teamsRU = {
     text: 'Познакомьтесь с нашей командой преподавателей',
     list: [
         {
-            img: './public/assets/img_teams/ekaterina.jpg', alt: 'Екатерина, основательница и преподаватель Aleksandrova Art Studio',
+            img: '/assets/img_teams/ekaterina.jpg', alt: 'Екатерина, основательница и преподаватель Aleksandrova Art Studio',
             name: 'Екатерина',
             text: 'Основатель и преподаватель Aleksandrova Art Studio.'
         },
         {
-            img: './public/assets/img_teams/kristina.jpg', alt: 'Кристина, преподаватель Aleksandrova Art Studio',
+            img: '/assets/img_teams/kristina.jpg', alt: 'Кристина, преподаватель Aleksandrova Art Studio',
             name: 'Кристина',
             text: 'Преподаватель Aleksandrova Art Studio.'
         },
